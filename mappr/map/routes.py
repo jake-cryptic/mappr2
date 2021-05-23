a177a76724702611552bb170816e72465e97bd6c
Jake Mcneill <jake-cryptic@users.noreply.github.com>
2021-05-23 17:02:07 +0100

    Added map offcanvas test page

--- mappr/map/routes.py
+++ mappr/map/routes.py
@@ -1,7 +1,6 @@
 from os import path, makedirs
 from flask import Blueprint, render_template, current_app
-from flask_login import current_user, login_required
-from ..models import db, User
+from flask_login import login_required
 
 map_bp = Blueprint("map_bp", __name__, template_folder="templates")
 
@@ -10,10 +9,16 @@ if not path.exists(current_app.config['MAP_FILES_DEST']):
 	makedirs(current_app.config['MAP_FILES_DEST'])
 
 
-@map_bp.route('/map', methods=['GET', 'POST'])
+@map_bp.route('/map', methods=['GET'])
 @login_required
 def mappr():
 	return render_template('map/map.html')
+
+
+@map_bp.route('/map2', methods=['GET'])
+@login_required
+def mappr2():
+	return render_template('map/mappr.html')
 
 
 @map_bp.route('/map/files', methods=['GET'])
