e55788689eab7bc3fb43f6343d1ae2ff18e25ca5
Jake Mcneill <jake-cryptic@users.noreply.github.com>
2021-05-24 17:56:40 +0100

    ImageProcessorThread now reads image exif data Exif data and Pillow used to correct image orientation File tables are now responsive Updated requirements.txt

--- mappr/gallery/threads.py
+++ mappr/gallery/threads.py
@@ -1,8 +1,31 @@
-import threading
+import threading, exifread
 from queue import Queue
 from time import sleep
 from PIL import Image
 
+
+def correct_image_orientation(im, tags):
+	if "Image Orientation" not in tags.keys():
+		return im
+
+	orientation = tags["Image Orientation"]
+	val = orientation.values
+
+	if 5 in val:
+		val += [4, 8]
+	if 7 in val:
+		val += [4, 6]
+	if 3 in val:
+		im = im.transpose(Image.ROTATE_180)
+	if 4 in val:
+		im = im.transpose(Image.FLIP_TOP_BOTTOM)
+	if 6 in val:
+		print("Rotating by 270 degrees.")
+		im = im.transpose(Image.ROTATE_270)
+	if 8 in val:
+		im = im.transpose(Image.ROTATE_90)
+
+	return im
 
 class ImageProcessorThread(threading.Thread):
 	def __init__(self, *args, **kwargs):
@@ -22,16 +45,24 @@ class ImageProcessorThread(threading.Thread):
 			if im_file is None:
 				break
 
+			# Read EXIF data from image
+			tags = {}
+			try:
+				with open(im_file, 'rb') as f:
+					tags = exifread.process_file(f, details=False)
+			except OSError:
+				print('Could not read exif data')
+
 			# Process the image here
 			try:
 				with Image.open(im_file) as im:
 					print(im_file, im.format, im.size, im.mode)
-					im.save(im_file + '.jpg', 'JPEG', quality=70, progressive=True)
-					im.save(im_file + '.webp', 'WEBP', lossless=False, method=6, quality=80)
+
+					f_im = correct_image_orientation(im, tags)
+					f_im.save(im_file + '.jpg', 'JPEG', quality=70, progressive=True)
+					f_im.save(im_file + '.webp', 'WEBP', lossless=False, method=6, quality=80)
 			except OSError:
 				print("Could not convert file", im_file)
-
-			# TODO: Figure out how to implement AVIF compression
 
 			self.input_queue.task_done()
 			sleep(1)
